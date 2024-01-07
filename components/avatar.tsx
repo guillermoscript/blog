6d0feef81c5ea1dc0514be38ffc5b8c14c97887f
guillermoscript <arepayquezo@gmail.com>
2024-01-07 15:55:52 -0400

    Replace next/image with img tag in avatar and cover-image components

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image"
 
 type Props = {
   name: string
@@ -8,7 +7,7 @@ type Props = {
 const Avatar = ({ name, picture }: Props) => {
   return (
     <div className="flex items-center">
-      <Image src={picture} className="w-12 h-12 rounded-full mr-4" alt={name} />
+      <img src={picture} className="w-12 h-12 rounded-full mr-4" alt={name} />
       <div className="text-xl font-bold">{name}</div>
     </div>
   )
