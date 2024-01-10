35e5ee3862137b10a9bd256cb30825f4cf2bc978
Guillermo Marin <52298929+guillermoscript@users.noreply.github.com>
2024-01-09 22:08:38 -0400

    Update component imports in index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
-import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
+import Container from '../components/blog/container'
+import MoreStories from '../components/blog/more-stories'
+import HeroPost from '../components/blog/hero-post'
+import Intro from '../components/blog/intro'
+import Layout from '../components/blog/layout'
 import { getAllPosts } from '../lib/api'
 import Head from 'next/head'
-import { CMS_NAME } from '../lib/constants'
 import Post from '../interfaces/post'
 
 type Props = {
@@ -19,7 +18,7 @@ export default function Index({ allPosts }: Props) {
     <>
       <Layout>
         <Head>
-          <title>{`Next.js Blog Example with ${CMS_NAME}`}</title>
+          <title>{`A Blog for copy & paste devs ;) `}</title>
         </Head>
         <Container>
           <Intro />
